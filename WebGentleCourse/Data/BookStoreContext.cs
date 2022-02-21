64c213f2e7727490a01771098129dafdac9025db
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-02-21 22:30:55 +0100

    Added EF Core

---
+++ WebGentleCourse/Data/BookStoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebGentleCourse.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+
+    }
+}
